6f0e24cc30673981f6ddfc1ae7e538fc9d122e56
Roman Byndas <48984151+rByndas@users.noreply.github.com>
2019-10-18 16:27:21 +0300

    Add files via upload

---
+++ podcluch/res/access.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+// Users data
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'id' => 'qqq47n66',
+		'name' => 'Admin',
+		'password' => '445549qo',
+		'page' => 'index.html'
+	),
+	'новый пользователь' => array(
+		'id' => '3065609q',
+		'name' => 'Новый Пользователь',
+		'password' => '4q41tw5t',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('qqq47n66');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array(
+);
+
+// End of file access.inc.php
---
+++ podcluch/res/search.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'6' => 'page-6.html',
+		'8' => 'reklama.html',
+		'18' => 'page-18.html',
+		'19' => 'page-19.html',
+		'20' => 'page-20.html',
+		'27' => 'page-27.html',
+		'28' => 'page-28.html',
+		'29' => 'page-29.html',
+		'30' => 'page-30.html',
+		'15' => 'page-15.html',
+		'16' => '------osb-3.html',
+		'32' => 'page-32.html',
+		'23' => 'page-23.html',
+		'24' => 'page-24.html',
+		'25' => 'page-25.html',
+		'5' => 'page-5.html',
+		'34' => 'page-34.html',
+		'9' => 'page-9.html',
+		'10' => 'page-10.html',
+		'11' => 'page-11.html',
+		'12' => 'page-12.html',
+		'13' => 'page-13.html',
+		'35' => 'page-35.html',
+		'37' => 'page-37.html',
+		'38' => 'page-38.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+
+// End of file search.inc.php
